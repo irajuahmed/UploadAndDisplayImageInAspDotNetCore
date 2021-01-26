eff9949c28b6f056997af24578c764ea574458b0
Raju Ahmed <rajuahmed.329@gmail.com>
2021-01-26 15:59:01 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadAndDisplayImageInAspDotNetCore.Data;
+using UploadAndDisplayImageInAspDotNetCore.Models;
+using UploadAndDisplayImageInAspDotNetCore.ViewModels;
+
+namespace UploadAndDisplayImageInAspDotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ApplicationDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public HomeController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            dbContext = context;
+            webHostEnvironment = hostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employee = await dbContext.Employees.ToListAsync();
+            return View(employee);
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> New(EmployeeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = UploadedFile(model);
+
+                Employee employee = new Employee
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    FullName = model.FirstName + " " + model.LastName,
+                    Gender = model.Gender,
+                    Age = model.Age,
+                    Office = model.Office,
+                    Position = model.Position,
+                    Salary = model.Salary,
+                    ProfilePicture = uniqueFileName,
+                };
+                dbContext.Add(employee);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        private string UploadedFile(EmployeeViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ProfileImage != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProfileImage.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadAndDisplayImageInAspDotNetCore.Models;
+
+namespace UploadAndDisplayImageInAspDotNetCore.Data
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base (options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
