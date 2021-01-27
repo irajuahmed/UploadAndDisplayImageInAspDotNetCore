944896ee51b0b5f3e363bf9e2bc26102981c67eb
Raju Ahmed <rajuahmed.329@gmail.com>
2021-01-27 09:49:21 +0600

    Database Migration from Local DB source to SQL server.

---
+++ Migrations/20210127034504_DbMigrations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UploadAndDisplayImageInAspDotNetCore.Migrations
+{
+    public partial class DbMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
